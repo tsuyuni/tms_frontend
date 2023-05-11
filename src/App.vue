e8d306a654f342a5656efc043e56b6bbf7350d6f
梅雨 <yuri.tsuchikawa@gmail.com>
2023-05-11 23:46:42 +0900

    Add Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+
+	import Axios from "axios"
+	import Login from "./components/Login.vue"
+
+	Axios.get("https://tms-api.herokuapp.com/users").then(res => {
+		console.log(res.data)
+	}).catch(err => {
+
+	})
+
 </script>
 
 <template>
-  
+	<Login />
 </template>
 
 <style scoped>
---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+	
+</script>
+
+<template>
+	<input type="text">
+	<input type="text">
+</template>
+
+<style scoped>
+	
+</style>
